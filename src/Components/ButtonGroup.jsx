7549a5a099f596d1e14a8bfeab8869b5d65b6437
Martin Kužel <martin.kuzel@inventi.cz>
2019-10-28 23:18:44 +0100

    styles beginnnig

--- src/Components/ButtonGroup.jsx
+++ src/Components/ButtonGroup.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+const DirButton = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 5rem;
+  height: 3rem;
+  background-color: silver;
+  border: 5px solid black;
+  margin: 0.2rem;
+  font-variant-caps: all-petite-caps; 
+  font-weight: bolder;
+  font-size: 2rem;
+  cursor: pointer;
+`;
+
 
 const ButtonGroup = ({setNewFilter}) => {
   return (
     <div>
-      <div onClick={() => { setNewFilter('IN') }}>In</div>
-      <div onClick={() => { setNewFilter('OUT') }}>Out</div>
-      <div onClick={() => { setNewFilter('ALL') }}>All</div>
+      <DirButton onClick={() => { setNewFilter('IN') }}>In</DirButton>
+      <DirButton onClick={() => { setNewFilter('OUT') }}>Out</DirButton>
+      <DirButton onClick={() => { setNewFilter('ALL') }}>All</DirButton>
     </div>
   );
 };
--- src/Components/NewTransactionButton.jsx
+++ src/Components/NewTransactionButton.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import {DirButton} from "./ButtonGroup";
+import styled from "styled-components";
+
+// Q: jak podedim z jine class ?
+// const AddButton = styled(DirButton)`
+//     border-radius: 15px;
+
+// `;
+
+const AddButton = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 3rem;
+  background-color: silver;
+  border: 5px solid black;
+  margin: 0.2rem;
+  font-variant-caps: all-petite-caps; 
+  font-weight: bolder;
+  font-size: 2rem;
+  border-radius: 1.5rem;
+  cursor: pointer;
+`;
+
+
 
 const NewTransactionButton = ({addTransaction}) => {
-  return <div 
+  return <AddButton 
     onClick={() => addTransaction(
       {description: "Vecere v restauraci", amount: 666, currency: "CZK", direction: "OUT"}
     )}>
-    NewTransaction
-    </div>;
+    New Transaction
+    </AddButton>;
 };
 
 export default NewTransactionButton;
